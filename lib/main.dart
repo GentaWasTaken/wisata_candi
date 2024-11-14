7750d474a57e4baa349b061034e0ecc3d8dd3e3f
GentaWasTaken <gentawibawapurnomo_2327250041@mhs.mdp.ac.id>
2024-11-14 12:20:27 +0700

    GridView.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_candi/data/candi_data.dart';
+import 'package:wisata_candi/screens/HomeScreen.dart';
 import 'package:wisata_candi/screens/detail_screen.dart';
 import 'package:wisata_candi/screens/profile_screen.dart';
+import 'package:wisata_candi/screens/search_screen.dart';
 import 'package:wisata_candi/screens/sign_in_screen.dart';
 import 'package:wisata_candi/screens/sign_up_screen.dart';
 
@@ -34,8 +36,10 @@ class MainApp extends StatelessWidget {
       ),
       //home: DetailScreen(candi: candiList[0]),
       //home: ProfileScreen(),
-      home: SignUpScreen(),
+      //home: SignUpScreen(),
       //home: SignInScreen(),
+      // home: SearchScreen(),
+      home: HomeScreen(),
     );
   }
 }
